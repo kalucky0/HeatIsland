65bb96688ce44b8a7e5fa8ca4e1e5a7b9c95d0ac
Piotrek Dziadkowiec <piotrek5994@gmail.com>
2024-06-08 15:57:17 +0200

    add map to project

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace HeatIsland
 {
@@ -9,15 +10,15 @@ namespace HeatIsland
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseSkiaSharp(true)
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
-#if DEBUG
-    		builder.Logging.AddDebug();
-#endif
+            builder.Logging.AddDebug();
+
 
             return builder.Build();
         }
