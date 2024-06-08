3ceb3277cb175f42b9774f9180cea049c90c69bf
kalucky0 <kalucki23@gmail.com>
2024-06-08 15:16:19 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HeatIsland
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
