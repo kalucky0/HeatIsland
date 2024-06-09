7c6ecf7d73749b348959c2a4ddc210eef9508a67
kalucky0 <kalucki23@gmail.com>
2024-06-09 03:47:14 +0200

    Fix tile orientation

--- Analyzer/TileRenderer.cs
+++ Analyzer/TileRenderer.cs
@@ -18,14 +18,7 @@ internal sealed class TileRenderer(Data data)
             Style = SKPaintStyle.Fill
         };
 
-        foreach (var point in data.GreenPoints)
-        {
-            var (x, y) = Utils.DecomposeId(point.Key);
-            var count = point.Value;
-
-            paint.Color = SKColor.FromHsl(0, 0, count * 1.3f);
-            canvas.DrawPoint(x, y, paint);
-        }
+        canvas.Scale(1, -1, 0, data.Height / 2f);
 
         foreach (var point in data.RoutePoints)
         {
@@ -34,9 +27,23 @@ internal sealed class TileRenderer(Data data)
 
             if (count < 4) continue;
 
-            paint.Color = SKColor.FromHsl(120, 255, count);
-            canvas.DrawCircle(x, y, 2, paint);
+            paint.Color = SKColor.FromHsl(10, 89, 66, 128);
+            canvas.DrawPoint(x, y, paint);
         }
+
+        foreach (var point in data.GreenPoints)
+        {
+            var (x, y) = Utils.DecomposeId(point.Key);
+            var count = point.Value;
+
+            paint.Color = SKColor.FromHsl(150, 50, count * 0.6f + 10f);
+            canvas.DrawPoint(x, y, paint);
+        }
+
+        paint.Color = SKColors.Black;
+        paint.Style = SKPaintStyle.Stroke;
+        paint.StrokeWidth = 2;
+        canvas.DrawRect(0, 0, data.Width, data.Height, paint);
 
         using var image = surface.Snapshot();
         using var imageData = image.Encode(SKEncodedImageFormat.Png, 100);
